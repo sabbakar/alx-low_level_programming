55b5ede0e873f5dd3ba8ebb1d0b4653252490d6f
sabbakar <sadeeqsas14@gmail.com>
2023-03-19 18:22:19 -0700

    one to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point of the program
+*
+* Description: This function generates the list of numbers from zero to nine and prints it out
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n = 0;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+for (n = 0; n < 10; n++) 
+putchar (n + '0');
+putchar('\n');
+
+ 
+
+return (0);
+}
+
