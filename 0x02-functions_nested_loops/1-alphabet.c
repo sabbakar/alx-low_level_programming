7d7a99ce8d6fe0dc80c6d7e6f73becb8fc1b066c
sabbakar <sadeeqsas14@gmail.com>
2023-03-21 17:00:34 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_alphabet - Entry point
+*Description: program that prints the alphabets
+*followed by a new line
+*Return: Exit int function with an int 0
+*/
+void print_alphabet(void)
+{
+int ap;
+
+
+for (ap = 'a'; ap <= 'z'; ap++)
+{
+putchar(ap);
+}
+putchar('\n');
+}
+
