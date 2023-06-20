5764370135e9d671127edd883397c58f973e214d
sabbakar <sadeeqsas14@gmail.com>
2023-06-19 18:01:06 -0700

    Task 3

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,2 +1,38 @@
 #include <stdio.h>
 #include <math.h>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+int div(int a, int b)
+{
+    if (b != 0)
+        return a / b;
+    else
+    {
+        printf("Error: Division by zero\n");
+        return 0;
+    }
+}
+
+int mul(int a, int b)
+{
+    return a * b;
+}
+
+int mod(int a, int b)
+{
+    if (b != 0)
+        return a % b;
+    else
+    {
+        printf("Error: Modulus by zero\n");
+        return 0;
+    }
+}
